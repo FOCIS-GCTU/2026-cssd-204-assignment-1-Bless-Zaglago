899255fd1a39b29b0161f0e8109e850bdb8d7208
Bless-Zaglago <2425402945@live.gctu.edu.gh>
2026-05-29 13:28:23 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Bless Mawumenyo Zaglago
+# STUDENT ID: 2425402945
+# Email: blesszaglago@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Bless Mawumenyo Zaglago, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This main function prints my initials in small and large stylized letters.
+    print()
+    print("...BMZ")
+    print()
+    print("...BBBBBBBBBB..........MMMM....MMMM........ZZZZZZZZZZZZ.....")
+    print("...BBBBBBBBBBB.........MMMM....MMMM........ZZZZZZZZZZZZ.....")
+    print("...BB......BBB.........MM.MM..MM.MM................ZZZ......")
+    print("...BB......BBB.........MM.MM..MM.MM...............ZZZ.......")
+    print("...BBBBBBBBBB..........MM.MM..MM.MM..............ZZZ........")
+    print("...BBBBBBBBBBB.........MM..MMMM..MM.............ZZZ.........")
+    print("...BB......BBB.........MM..MMMM..MM............ZZZ..........")
+    print("...BB......BBB.........MM...MM...MM...........ZZZ...........")
+    print("...BBBBBBBBBBB....**...MM...MM...MM...**...ZZZZZZZZZZZZ...**")
+    print("...BBBBBBBBBB.....**...MM...MM...MM...**...ZZZZZZZZZZZZ...**")
+    print()
 
 
 main()
